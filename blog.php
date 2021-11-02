8ed8ef36c4641845e102aabcc58c4ef71f19bff7
Tom Phillips <eflips@yahoo.com>
2021-11-01 19:18:35 -0700

    Spacing better HTML

---
+++ blog.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/nat4hnx.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    
+    <!-- Site Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#f26522">
+    <meta name="msapplication-TileColor" content="#603cba">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Blog</title>
+    <meta name="description" content="Atomic is known for building fast and effective websites for small businesses. We offer graphic design, site design, development and web hosting."/>
+    <meta name="keywords" content="Web Development, Hosting, Web Design, graphic design, brochures, catalogs, websites, logos." />
+    <meta name="author" content="Atomic/TomPhillips" />
+
+    <!--- Open Graph data -->
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Where design and technoolgy intersect." />
+    <meta property="og:description" content="Atomic is known for building fast and effective websites for small businesses. We offer graphic design, site design, development and web hosting." />
+    <meta property="og:url" content="https://atomicpx.com/" />
+    <meta property="og:site_name" content="Atomic" />
+    <meta property="article:publisher" content="https://www.facebook.com/amikasBridgeHousing" />
+    <meta property="article:modified_time" content="2021-11-01T04:24:03+00:00" />
+    <meta property="og:image" content="https://atomicpx.com/images/OG-image.jpg" />
+    <meta property="og:image:width" content="2560" />
+    <meta property="og:image:height" content="1751" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Where design and technology intersect" />
+    <meta name="twitter:description" content="Atomic is known for building fast and effective websites for small businesses. We offer graphic design, site design, development and web hosting" />
+    <meta name="twitter:image" content="https://atomicpx.com/images/OG-twitter.jpg" />
+    <meta name="twitter:site" content="@tripdog" />
+    <meta name="twitter:label1" content="Est. reading time" />
+    <meta name="twitter:data1" content="3 minutes" />
+    
+    <style>
+    .jumbotron {width: 100%;height: 250px !important;}
+        .jumbotron h2 {padding-bottom: 0;}
+    </style>
+</head>
+<body>
+<?php include("inner-header.php"); ?>
+<!--Section-1-->
+<section class="section-1">
+        <div class="jumbotron d-flex align-items-center">
+            <div class="gradient"></div>
+            <div class="container-fluid content">
+                <h1 data-aos="fade-right" data-aos-delay="300">Welcome to more.</h1>
+                <h2 data-aos="fade-left" data-aos-delay="300">the multipurpose psd wordrpess theme</h2>
+            </div>
+            <!--container-fluid end-->
+        </div>
+    </section>
+    
+    <section class="blog">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-12">
+                    <h3>Blog Articles</h3>
+                </div>
+            </div>
+        </div>    
+        <hr>
+        <div class="container">
+            <!--/row-->
+            <div class="row">
+                <div class="col-lg-4 col-sm-12 col-12 box-1"  data-aos="fade-right" data-aos-delay="300">
+                    <figure class="figure">
+                        <a href="wikipedia_for_the_win.php"><img src="images/blog-1.jpg" class="figure-img img-fluid" alt="blog"></a>
+                        <figcaption class="figure-caption">
+                            <h2><a href="blog.php">How Wikipedia can help legitimize your charity.</a></h2>
+                            <p>It can be difficult to gain legitimacy when starting a new charitable venture. But with the help of Wikipedia, you can help build your brand's story.</p>
+                            <a href="wikipedia_for_the_win.php" class="btn btn-success">+ more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-lg-8 col-sm-12 col-12"  data-aos="fade-left" data-aos-delay="300">
+                    <div class="row">
+                        <div class="col-sm-6 col-12 box-2">
+                            <figure class="figure">
+                                <a href="blog.php"><img src="images/blog-2.jpg" class="figure-img img-fluid" alt="blog"></a>
+                            </figure>
+                        </div>
+                        <div class="col-sm-6 col-12 box-3">
+                            <figcaption class="figure-caption">
+                                <h4><a href="do_you_need_a_cms.php">Do you really <em>need</em> a CMS?</a></h4>
+                                <p>The answer isn't always yes. WordPress and other CMS platforms have a great many benefits, but they have some disadvantages as well.</p>
+                                <a href="do_you_need_a_cms.php" class="btn btn-success btn-more">+ more</a>
+                            </figcaption>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-12 box-4">
+                            <figure class="figure">
+                                <a href="blog_single.html"><img src="images/blog-3.jpg" class="figure-img img-fluid" alt="blog"></a>
+                            </figure>
+                        </div>
+                        <div class="col-sm-6 col-12 box-5">
+                            <figcaption class="figure-caption">
+                                <h4><a href="make_my_logo_bigger.php">Make your logo bigger?</a></h4>
+                                <p>In the design game, a bigger logo isn't always better. Most often a modest, sophisticated approach is the way to go. </p>
+                                <a href="make_my_logo_bigger.php" class="btn btn-success btn-more">+ more</a>
+                            </figcaption>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-12 box-6">
+                            <figure class="figure">
+                                <a href="blog.php"><img src="images/blog-4.jpg" class="figure-img img-fluid" alt="blog"></a>
+                            </figure>
+                        </div>
+                        <div class="col-sm-6 col-12 box-7">
+                            <figcaption class="figure-caption">
+                                <h4><a href="future_of_pring.php">Is print dying?</a></h4>
+                                <p>Sometimes it may seem that way, but it's hard to compete digitally with the tactile quality of paper. Print has it's place and it's here to stay.</p>
+                                <a href="future_of_pring.php" class="btn btn-success btn-more">+ more</a>
+                            </figcaption>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--container-->
+    </section>
+    <!-- Now the footer-->
+    <?php include("footer.php"); ?>
