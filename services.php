ab091a8f8bd98d7128103b1345912840bc201382
tripdog <eflips@yahoo.com>
2021-12-09 21:02:20 -0800

    changes to bootstrap.css and main.css

--- services.php
+++ services.php
@@ -7,11 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/nat4hnx.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/main.css">
 
@@ -97,7 +96,10 @@
                         <div class="col-sm-11 col-12">
                             <h3>Graphic Design</h3>
                             <p>I started my career as a graphic designer and it's still something I'm passionate about. I design magazines from cover to cover, brochures, direct-mail pieces, logos, stationery, posters and banners. I have great connections with local San Diego printers and as well as trade printers for those on a tight budget.</p>
-                            <p>A favorite part of my job is learning about new businesses or charities and the goals of each. Once I learn your story, then I get to go about the business of telling it in a compelling visual way.</p><a class="contact-link" href="contact.php">Let's talk about your story.</a>
+                            <p>A favorite part of my job is learning about new businesses or charities and the goals of each. Once I learn your story, then I get to go about the business of telling it in a compelling visual way.</p>
+                                <form action="contact.php">
+                                    <input class="btn btn-primary" type="submit" value="Get a Quote" />
+                                </form>
                         </div>
                     </div>
                 </div>
@@ -110,6 +112,9 @@
                             <h3><a href="blog.html">Web Development</a></h3>
                             <p>Web development is often about problem solving and learning new technologies. I excell at crafting websites because every project brings its own challenges and I enjoy figuring out how to put the pieces together. I specialize in sites that are grounded in solid design principals and communicate your service, product or charity in a way that makes sense to consumers.</p>
                             <p>I am well versed in best practices for SEO and user interface design and I can create/configure <strong>Google Analytics</strong> and <strong>Google Tag Manager</strong> accounts for you so that you can track user behavior in your app or website to make it even more effective. I can build with <strong>WordPress</strong> or a site comprised of <strong>JavaScript, PHP, HTML and CSS.</strong></p>
+                                <form action="contact.php">
+                                    <input class="btn btn-primary" type="submit" value="Get a Quote" />
+                                </form>
                         </div>
                     </div>
                 </div>
@@ -121,7 +126,11 @@
                         <div class="col-sm-11 col-12">
                             <h3>Hosting & Maintenance</h3>
                             <p>I offer hosting to all of my clients. You don't have to host with Atomic, but I can offer competitive prices and I offer a 10% discounted rate for maintenance on sites that are hosted with Atomic. The base plan is $20 per month and it includes: 20 GB Web Space •100,000 Visits Monthly •Unmetered Traffic •Free SSL Cert •Daily Backup, •Free CDN •Free Email, •Enhanced Security, •Ecommerce Enabled, •Managed WordPress, •Out-of-the-box Caching, •Unlimited Databases, *100% renewable energy match and on demand backups.</p>
-                            <p>I purchase hosting in bulk from a top level provider so it's dependable and cost effective for small businesses. If you have a need for a more custom package, I can provide that as well.</p><a class="contact-link" href="contact.php">Host your site with Atomic</a>
+                            <p>I purchase hosting in bulk from a top level provider so it's dependable and cost effective for small businesses. If you have a need for a more custom package, I can provide that as well.</p>
+                                <form action="contact.php">
+                                    <input class="btn btn-primary" type="submit" value="Get Hosting" />
+                                </form>
+
                         </div>
                     </div>
                 </div>
